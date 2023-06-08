068e6d3781284b9e6190a34c01b26127a5bfbbd1
sosos11111 <135943151+sosos11111@users.noreply.github.com>
2023-06-08 15:35:07 +0900

    initial commit

---
+++ js/script.js
@@ -0,0 +1,287 @@
+$(function () {
+
+    var is_clicked = false;
+
+    $('#portfolio_img1').click(function (e) {
+        e.preventDefault();
+        $('#homepage').addClass('block');
+    });
+    $('#portfolio_img2').click(function (e) {
+        e.preventDefault();
+        $('#branding').addClass('block');
+    });
+    $('#portfolio_img3').click(function (e) {
+        e.preventDefault();
+        $('#package').addClass('block');
+    });
+    $('#portfolio_img4').click(function (e) {
+        e.preventDefault();
+        $('#poster').addClass('block');
+    });
+    $('#portfolio_img5').click(function (e) {
+        e.preventDefault();
+        $('#banner').addClass('block');
+    });
+    $('.close').click(function (e) {
+        e.preventDefault();
+        $('.modal').removeClass('block');
+    });
+
+    var topButton = document.querySelector('#top-button');
+    var profileButton = document.querySelector('#profile-button');
+    var skillButton = document.querySelector('#skill-button');
+    var portfolioButton = document.querySelector('#portfolio-button');
+    var noticeButton = document.querySelector('#notice-button');
+
+    topButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        window.scrollTo({ left: 0, top: 0, behavior: 'smooth' });
+        // 페이지 고정
+        page = 1;
+    });
+    profileButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        window.scrollTo({ left: 1900, top: 0, behavior: 'smooth' });
+        // 페이지 고정
+        page = 2;
+    });
+    skillButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        window.scrollTo({ left: 3800, top: 0, behavior: 'smooth' });
+        // 페이지 고정
+        page = 3;
+    });
+    portfolioButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        window.scrollTo({ left: 5800, top: 0, behavior: 'smooth' });
+        // 페이지 고정
+        page = 4;
+    });
+    noticeButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        window.scrollTo({ left: 7800, top: 0, behavior: 'smooth' });
+        // 페이지 고정
+        page = 5;
+    });
+
+
+    // --------------------------------------------------------------------------
+    // scroll
+    var $window = $(window),
+        $html = $('html'),
+        windowWidth = $window.width(),
+        // pageIndex = 0,
+        pageIndex = 1,
+        lastPageIndex = $('.page').length - 1;
+
+    pageIndex = Math.round($window.scrollLeft() / windowWidth);
+    $html.animate({ scrollLeft: pageIndex * windowWidth }, 10);
+
+    var page = 1;
+
+    $(window).on("wheel", function (e) {
+        if ($html.is(":animated")) return;
+        if (e.originalEvent.deltaY > 0) {
+            if (page == 5) return;
+            page++;
+        } else if (e.originalEvent.deltaY < 0) {
+            if (page == 1) return;
+            page--;
+        }
+        var posLeft = (page - 1) * $(window).width();
+        $html.animate({ scrollLeft: posLeft });
+        console.log($(window).width());
+        console.log(posLeft, page);
+    });
+
+    window.addEventListener('resize', function (e) {
+        e.preventDefault();
+        windowWidth = $window.width();
+    });
+
+    // --------------------------------------------------------------------------
+    // homepage popup
+
+    var slides = document.querySelector('#page-container1 > li'),
+        slide = document.querySelector('#page-container1'),
+        photoCount = slides.length,
+        duration = 400,
+        photoIndex = 0;
+
+    // 슬라이드 버튼 클릭 이벤트
+    document.querySelector("#next1").addEventListener("click", nextSlideImage);
+    document.querySelector("#prev1").addEventListener("click", prevSlideImage);
+
+    // 다음 사진으로 슬라이드
+    function nextSlideImage() {
+        photoIndex++;
+        photoIndex %= photoCount;
+        slide.style.left = "-100%";
+        slide.style.transition = duration + "ms";
+        window.setTimeout(() => {
+            slide.appendChild(slide.firstElementChild);
+            slide.removeAttribute("style");
+        }, duration);
+    }
+    // 이전 사진으로 슬라이드
+    function prevSlideImage() {
+        photoIndex--;
+        photoIndex %= photoCount;
+        slide.insertBefore(slide.lastElementChild, slide.firstChild);
+        slide.style.left = "-100%";
+        slide.style.transition = "0ms";
+        window.setTimeout(() => {
+            slide.style.left = 0;
+            slide.style.transition = duration + "ms";
+        });
+    }
+
+    // --------------------------------------------------------------------------
+    // branding popup
+
+    var slides2 = document.querySelector('#page-container2 > li'),
+        slide2 = document.querySelector('#page-container2'),
+        photoCount2 = slides2.length,
+        duration2 = 400,
+        photoIndex2 = 0;
+
+    // 슬라이드 버튼 클릭 이벤트
+    document.querySelector("#next2").addEventListener("click", nextSlideImage2);
+    document.querySelector("#prev2").addEventListener("click", prevSlideImage2);
+
+    // 다음 사진으로 슬라이드
+    function nextSlideImage2() {
+        photoIndex2++;
+        photoIndex2 %= photoCount2;
+        slide2.style.left = "-100%";
+        slide2.style.transition = duration2 + "ms";
+        window.setTimeout(() => {
+            slide2.appendChild(slide2.firstElementChild);
+            slide2.removeAttribute("style");
+        }, duration2);
+    }
+    // 이전 사진으로 슬라이드
+    function prevSlideImage2() {
+        photoIndex2--;
+        photoIndex2 %= photoCount2;
+        slide2.insertBefore(slide2.lastElementChild, slide2.firstChild);
+        slide2.style.left = "-100%";
+        slide2.style.transition = "0ms";
+        window.setTimeout(() => {
+            slide2.style.left = 0;
+            slide2.style.transition = duration2 + "ms";
+        });
+    }
+
+
+    // --------------------------------------------------------------------------
+    // package popup
+
+    var slides3 = document.querySelector('#page-container3 > li'),
+        slide3 = document.querySelector('#page-container3'),
+        photoCount3 = slides3.length,
+        duration3 = 400,
+        photoIndex3 = 0;
+
+    // 슬라이드 버튼 클릭 이벤트
+    document.querySelector("#next3").addEventListener("click", nextSlideImage3);
+    document.querySelector("#prev3").addEventListener("click", prevSlideImage3);
+
+    // 다음 사진으로 슬라이드
+    function nextSlideImage3() {
+        photoIndex3++;
+        photoIndex3 %= photoCount3;
+        slide3.style.left = "-100%";
+        slide3.style.transition = duration3 + "ms";
+        window.setTimeout(() => {
+            slide3.appendChild(slide3.firstElementChild);
+            slide3.removeAttribute("style");
+        }, duration3);
+    }
+    // 이전 사진으로 슬라이드
+    function prevSlideImage3() {
+        photoIndex3--;
+        photoIndex3 %= photoCount3;
+        slide3.insertBefore(slide3.lastElementChild, slide3.firstChild);
+        slide3.style.left = "-100%";
+        slide3.style.transition = "0ms";
+        window.setTimeout(() => {
+            slide3.style.left = 0;
+            slide3.style.transition = duration3 + "ms";
+        });
+    }
+
+    // --------------------------------------------------------------------------
+    // poster popup
+
+    var slides4 = document.querySelector('#page-container4 > li'),
+        slide4 = document.querySelector('#page-container4'),
+        photoCount4 = slides4.length,
+        duration4 = 400,
+        photoIndex4 = 0;
+
+    // 슬라이드 버튼 클릭 이벤트
+    document.querySelector("#next4").addEventListener("click", nextSlideImage4);
+    document.querySelector("#prev4").addEventListener("click", prevSlideImage4);
+
+    // 다음 사진으로 슬라이드
+    function nextSlideImage4() {
+        photoIndex4++;
+        photoIndex4 %= photoCount4;
+        slide4.style.left = "-100%";
+        slide4.style.transition = duration4 + "ms";
+        window.setTimeout(() => {
+            slide4.appendChild(slide4.firstElementChild);
+            slide4.removeAttribute("style");
+        }, duration4);
+    }
+    // 이전 사진으로 슬라이드
+    function prevSlideImage4() {
+        photoIndex4--;
+        photoIndex4 %= photoCount4;
+        slide4.insertBefore(slide4.lastElementChild, slide4.firstChild);
+        slide4.style.left = "-100%";
+        slide4.style.transition = "0ms";
+        window.setTimeout(() => {
+            slide4.style.left = 0;
+            slide4.style.transition = duration4 + "ms";
+        });
+    }
+
+    // --------------------------------------------------------------------------
+    // banner popup
+
+    var slides5 = document.querySelector('#page-container5 > li'),
+        slide5 = document.querySelector('#page-container5'),
+        photoCount5 = slides5.length,
+        duration5 = 400,
+        photoIndex5 = 0;
+
+    // 슬라이드 버튼 클릭 이벤트
+    document.querySelector("#next5").addEventListener("click", nextSlideImage5);
+    document.querySelector("#prev5").addEventListener("click", prevSlideImage5);
+
+    // 다음 사진으로 슬라이드
+    function nextSlideImage5() {
+        photoIndex5++;
+        photoIndex5 %= photoCount5;
+        slide5.style.left = "-100%";
+        slide5.style.transition = duration5 + "ms";
+        window.setTimeout(() => {
+            slide5.appendChild(slide5.firstElementChild);
+            slide5.removeAttribute("style");
+        }, duration5);
+    }
+    // 이전 사진으로 슬라이드
+    function prevSlideImage5() {
+        photoIndex5--;
+        photoIndex5 %= photoCount5;
+        slide5.insertBefore(slide5.lastElementChild, slide5.firstChild);
+        slide5.style.left = "-100%";
+        slide5.style.transition = "0ms";
+        window.setTimeout(() => {
+            slide5.style.left = 0;
+            slide5.style.transition = duration5 + "ms";
+        });
+    }
+});
